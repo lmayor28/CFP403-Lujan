d2c6cb2e0b975dddeee7f8e6d84a333c137f78bc
matinara <carmatnar@gmail.com>
2025-05-19 16:03:12 -0300

    m

---
+++ Naranjo Matias/JavaScript/Index.js
@@ -0,0 +1,60 @@
+ // console.log ("Hola Mundo") 
+
+let nombre = "matias"
+console.log(nombre)
+
+let edad = "52"
+console.log(edad)
+
+let esEstudiante = true
+console.log(esEstudiante)
+
+let num1 = 1
+let num2 = 2
+let suma = (num1+num2)
+let resta = (num1-num2)
+let multiplic = (num1*num2)
+let div = (num1/num2)
+console.log(suma,resta,multiplic,div)
+
+let saludo = "Hola"
+let usuario = "Mundo"
+let mensaje = `${saludo} ${usuario}` 
+console.log(mensaje)
+
+let variable 
+console.log(variable)
+console.log(typeof variable)
+
+let mivariable = null
+console.log(mivariable)
+console.log(typeof mivariable)
+
+let contador = (10)
+let cont = (5)
+contador += cont
+console.log(contador)
+
+let numer = 5
+let numero = "5"
+
+console.log(numer==numero)
+console.log(numer===numero)
+
+let a = true
+let b = false 
+
+console.log(a&&b,a||b )
+
+let c = true
+console.log(c)
+console.log(!c)
+
+let d = 3
+
+if (d>0) {console.log("numero positivo")
+
+} else if (d==0) {console.log("numero es cero")
+
+} else { console.log("numero es negativo")}
+
