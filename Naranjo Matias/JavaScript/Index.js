84341dbbaccffe27de4b15fedbbc1d1cfa5e108b
matinara <carmatnar@gmail.com>
2025-05-22 20:18:41 -0300

    m

--- Naranjo Matias/JavaScript/Index.js
+++ Naranjo Matias/JavaScript/Index.js
@@ -58,3 +58,100 @@ if (d>0) {console.log("numero positivo")
 
 } else { console.log("numero es negativo")}
 
+
+let semaforo = "verde"
+
+switch (semaforo) {
+   
+    case "rojo":
+        console.log("detente")
+    break
+
+    case "amarillo":
+        console.log("precaucion")
+    break
+
+    case "verde":
+        console.log("avanza")
+    break
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+  }
+
+  let i = 1
+
+while (i <= 10) {
+  console.log(i)
+  i++
+}
+
+for(let i = 0 ; i <= 20; i++) {
+    console.log(i)
+
+}
+
+let frutas = ["manzana", "banana", "naranja", "uva"]
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+  }
+
+function saludar(nombre){
+    console.log("hola ", nombre)
+}
+
+saludar("matias")
+saludar("luis")
+
+function sumar(a , b){
+    return a + b 
+}
+   let resultado = sumar(2,3)
+
+   console.log(resultado)
+
+const sumando = (a, b) => a + b
+
+   let valor  = sumar(5, 3)
+   console.log(valor)
+
+function multiplicacion(A , b) {
+    
+}
+let nada = multiplicacion(2,3)
+
+console.log(nada)
+
+let fruits = ["manzana" , "banana" , "mandarina", "frutilla", "naranja", "ciruela" , "kiwi", "pera" , "higo"  ]
+
+console.log(fruits)
+
+console.log(fruits[0])
+
+console.log(fruits[fruits.length - 1])
+
+fruits.push("durazno")
+
+console.log(fruits)
+
+fruits.pop()
+
+console.log(fruits)
+
+for (let i = 0 ; i < fruits.length; i++) {
+    console.log(fruits[i]);
+  }
+
+
+  let persona = {
+    nombre :"matias",
+    edad :39,
+    ciudad : "lujan"
+  }
+
+  console.log(persona)
+
+  console.log(persona.nombre)
