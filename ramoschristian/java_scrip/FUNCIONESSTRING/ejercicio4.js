402266816db4ed04c9e8445f5a05b899899732f3
christianramos367 <ramoschristianmichel@gmail.com>
2025-06-06 15:50:47 -0300

    tp4

---
+++ ramoschristian/java_scrip/FUNCIONESSTRING/ejercicio4.js
@@ -0,0 +1,67 @@
+// let deber ={
+//     descripcion:"pasear al perro una hora por dia",
+//     estado: false,
+//     nombre: "pasear perro"
+// }
+// let deber1 ={
+//     descripcion:"sacar la basura de lunes a viernes",
+//     estado:false,
+//     nombre:"basura"
+// }
+
+// let deber2 ={
+//     descripcion:"cortar el pastoa 4cm del suelo",
+//     estado: false,
+//     nombre:"cortar pasto"
+// }
+
+// let deber3 ={
+//     descripcion:"lavar los platos y las ollas",
+//     estado:false,
+//     nombre:"lavar platos"
+// }
+
+
+// let sistema_gestion ={
+//     tareas:[],
+    
+//     agregartarea:function(tarea){
+//      this.tareas.push(tarea)
+//     },
+
+//     marcar_tarea_finalizada: function(nombre){
+//         let tarea_objetivo 
+//         this.tareas.forEach(function(tarea) {
+//           if(tarea.nombre== nombre){
+//             tarea.estado=true 
+//             tarea_objetivo=tarea
+//           };
+//         });
+//         if (tarea_objetivo){
+//             console.log("se borro la tarea")
+//         }
+
+//     },
+
+//     lista_de_tareas_pendientes : function(){
+//          return this.tarea
+//     },
+
+//     borrar_tarea: function(nombre) {
+//         delete this.tarea
+//     },
+
+//     obtener_tareas: function(){
+//         return this.tareas
+//     },
+//     obtener_tarea: function(nombre){
+//         return this.tarea
+//     }
+
+// }
+// console.log(sistema_gestion.obtener_tareas())
+
+// sistema_gestion.agregartarea(tarea)
+
+
+// console.log(sistema_gestion.obtener_tareas())
